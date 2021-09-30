4af4eebe5f2f24f1fab9ad905bfddbd3fad2f381
TrentVarnes <varnestw@g.cocfc.edu>
2021-09-30 15:53:25 -0400

    completed lab5

--- labs/lab5/lab5.py
+++ labs/lab5/lab5.py
@@ -1,7 +1,8 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Trent Varnes
+lab5.py
 """
+import math
 
 from graphics import *
 
@@ -24,7 +25,22 @@ def triangle():
     win = GraphWin("Draw a Triangle", win_width, win_height)
 
     # Add code here to accept the mouse clicks, draw the triangle.
-    # and display its area in the graphics window.
+    p1 = win.getMouse()
+    p2 = win.getMouse()
+    p3 = win.getMouse()
+    triangle1 = Polygon(p1, p2, p3)
+    triangle1.draw(win)
+    a = math.sqrt((p1.getX() - p2.getX())**2 + (p1.getY() - p2.getY())**2)
+    b = math.sqrt((p2.getX() - p3.getX())**2 + (p2.getY() - p3.getY())**2)
+    c = math.sqrt((p1.getX() - p3.getX())**2 + (p1.getY() - p3.getY())**2)
+    perimeter = a + b + c
+    perimeter_text = Text(Point(200, 390), "The perimeter is: " + str(perimeter))
+    perimeter_text.draw(win)
+    s = (a + b + c) // 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    area_text = Text(Point(200, 370), "The area is: " + str(area))
+    area_text.draw(win)
+
 
     # Wait for another click to exit
     win.getMouse()
@@ -71,15 +87,65 @@ def color_shape():
     green_text.draw(win)
     blue_text.draw(win)
 
+    #all code here
+    red_box = Entry(Point(win_height / 2 - 50, win_width / 2 + 40), 5)
+    red_box.draw(win)
+    green_box = Entry(Point(win_height / 2 - 50, win_width / 2 + 70), 5)
+    green_box.draw(win)
+    blue_box = Entry(Point(win_height / 2 - 50, win_width / 2 + 100), 5)
+    blue_box.draw(win)
+    for _ in range(5):
+        win.getMouse()
+        red = int(red_box.getText())
+        green = int(green_box.getText())
+        blue = int(blue_box.getText())
+        color = color_rgb(red, green, blue)
+        shape.setFill(color)
+
     # Wait for another click to exit
     win.getMouse()
     win.close()
 
 
+def process_string():
+    string1 = input("enter a string ")
+    print(string1[0])
+    print(string1[-1])
+    print(string1[2:6])
+    print(string1[:])
+    print(string1[0:4] * 10)
+    for c in string1:
+        print(c)
+    print(len(string1))
+
+
+def process_list():
+    pt = Point(5, 10)
+    values = [5, "hi", 2.5, "there", pt, "7.2"]
+    x = values[1] + values[3]
+    print(x)
+    x = values[0] + values[2]
+    print(x)
+    x = values[1] * 5
+    print(x)
+    x = [values[2], values[3], pt]
+    print(x)
+    x = [values[2], values[3], values[0]]
+    print(x)
+    x = [values[2], values[3], float(values[5])]
+    print(x)
+    x =values[0] + values[2] + float(values[5])
+    print(x)
+    x = len(values)
+    print(x)
+
+
 def main():
-    # target()
-    # triangle()
-    # color_shape()
+    target()
+    triangle()
+    color_shape()
+    process_string()
+    process_list()
     pass
 
 
